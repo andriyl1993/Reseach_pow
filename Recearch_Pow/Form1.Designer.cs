7cf82f8e20d69540e85fb8d32f91377f16ee2c57
A.Letsyk <a.letsyk@kit-xxi.com.ua>
2017-01-13 19:41:39 +0200

    Добавьте файлы проекта.

---
+++ Recearch_Pow/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Recearch_Pow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxStandard = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBoxModified = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxRandomNumbers = new System.Windows.Forms.TextBox();
+            this.radioButtonAll = new System.Windows.Forms.RadioButton();
+            this.radioButtonRand = new System.Windows.Forms.RadioButton();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.textBoxBits = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxStandard
+            // 
+            this.checkedListBoxStandard.FormattingEnabled = true;
+            this.checkedListBoxStandard.Location = new System.Drawing.Point(12, 42);
+            this.checkedListBoxStandard.Name = "checkedListBoxStandard";
+            this.checkedListBoxStandard.Size = new System.Drawing.Size(172, 319);
+            this.checkedListBoxStandard.TabIndex = 0;
+            // 
+            // checkedListBoxModified
+            // 
+            this.checkedListBoxModified.FormattingEnabled = true;
+            this.checkedListBoxModified.Items.AddRange(new object[] {
+            "Prime Numbers",
+            "Pow_Table_0"});
+            this.checkedListBoxModified.Location = new System.Drawing.Point(200, 42);
+            this.checkedListBoxModified.Name = "checkedListBoxModified";
+            this.checkedListBoxModified.Size = new System.Drawing.Size(172, 319);
+            this.checkedListBoxModified.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Standard Methods";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Modified Methods";
+            // 
+            // textBoxRandomNumbers
+            // 
+            this.textBoxRandomNumbers.Location = new System.Drawing.Point(3, 49);
+            this.textBoxRandomNumbers.Name = "textBoxRandomNumbers";
+            this.textBoxRandomNumbers.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRandomNumbers.TabIndex = 4;
+            this.textBoxRandomNumbers.Text = "10";
+            // 
+            // radioButtonAll
+            // 
+            this.radioButtonAll.AutoSize = true;
+            this.radioButtonAll.Location = new System.Drawing.Point(3, 3);
+            this.radioButtonAll.Name = "radioButtonAll";
+            this.radioButtonAll.Size = new System.Drawing.Size(79, 17);
+            this.radioButtonAll.TabIndex = 5;
+            this.radioButtonAll.Text = "All numbers";
+            this.radioButtonAll.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRand
+            // 
+            this.radioButtonRand.AutoSize = true;
+            this.radioButtonRand.Checked = true;
+            this.radioButtonRand.Location = new System.Drawing.Point(3, 26);
+            this.radioButtonRand.Name = "radioButtonRand";
+            this.radioButtonRand.Size = new System.Drawing.Size(108, 17);
+            this.radioButtonRand.TabIndex = 6;
+            this.radioButtonRand.TabStop = true;
+            this.radioButtonRand.Text = "Random numbers";
+            this.radioButtonRand.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.radioButtonAll);
+            this.flowLayoutPanel1.Controls.Add(this.radioButtonRand);
+            this.flowLayoutPanel1.Controls.Add(this.textBoxRandomNumbers);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(381, 90);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(183, 83);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(378, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Max length";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(378, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Number of bits";
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Location = new System.Drawing.Point(464, 35);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLength.TabIndex = 10;
+            this.textBoxLength.Text = "7";
+            // 
+            // textBoxBits
+            // 
+            this.textBoxBits.Location = new System.Drawing.Point(464, 64);
+            this.textBoxBits.Name = "textBoxBits";
+            this.textBoxBits.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBits.TabIndex = 11;
+            this.textBoxBits.Text = "40";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(384, 180);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 372);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxBits);
+            this.Controls.Add(this.textBoxLength);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBoxModified);
+            this.Controls.Add(this.checkedListBoxStandard);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxStandard;
+        private System.Windows.Forms.CheckedListBox checkedListBoxModified;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxRandomNumbers;
+        private System.Windows.Forms.RadioButton radioButtonAll;
+        private System.Windows.Forms.RadioButton radioButtonRand;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.TextBox textBoxBits;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ Recearch_Pow/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Recearch_Pow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            checkedListBoxModified.SetItemChecked(0, true);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            BigInteger length, n;
+            BigInteger.TryParse(textBoxLength.Text, out length);
+            BigInteger.TryParse(textBoxBits.Text, out n);
+
+            BigInteger max_number = new BigInteger(Math.Pow(2, (double)n));
+
+            List<BigInteger> numbers = new List<BigInteger>();
+
+            bool isRand = radioButtonRand.Checked;
+
+            if (isRand)
+            {
+                int rand_count = Int32.Parse(textBoxRandomNumbers.Text);
+                Random r = new Random();
+
+                for (int i = 0; i < rand_count; i++)
+                    numbers.Add(r.Next() % max_number);
+            }
+
+            for (int i = 0; i < checkedListBoxModified.Items.Count; i++)
+            {
+                if (checkedListBoxModified.GetItemChecked(i))
+                    Methods.MethodsList[i](length, n, numbers, isRand);
+            }
+        }
+    }
+}
